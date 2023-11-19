748055c395b9c8ff611b64315b94abdd88ead8c8
sarthak sharma <sharma.sarthak222872@gmail.com>
2023-11-19 15:37:29 +0530

    Delete extra css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import { AppHome, Client1Home, Client2Home }from './pages/Index';
 
 export const AppRouter: FC = () => {
